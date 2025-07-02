5108f667d4fbc9c55abbd9ab7b6d14115a0f030c
Mir Raisul Tanvir <tanvirgraphic@gmail.com>
2025-07-03 00:01:04 +0600

    cv update

--- components/nav/Heading.tsx
+++ components/nav/Heading.tsx
@@ -8,7 +8,7 @@ export const Heading = () => {
       <MyLinks />
       {/* <div className={styles.headingButtons}> */}
 
-      <OutlineButton onClick={() => window.open("https://drive.google.com/file/d/17ROA_no-nIbbsN-KHUNGPHjO5U-dWeJz/view")}>
+      <OutlineButton onClick={() => window.open("https://drive.google.com/file/d/1TcrCu2GxAF_Xjs_AEKGdiDwLTaaCmA7F/view")}>
         My resume
       </OutlineButton>
       {/* </div> */}
@@ -16,15 +16,4 @@ export const Heading = () => {
   );
 };
 
-{
-  /* <a href="https://www.buymeacoffee.com/jcdevz" target="_blank">
-  <img
-    src="https://cdn.buymeacoffee.com/buttons/v2/arial-violet.png"
-    alt="Buy Me A Coffee"
-    style={{
-      height: "46px !important",
-      width: "185px !important",
-    }}
-  />
-</a> */
-}
+
